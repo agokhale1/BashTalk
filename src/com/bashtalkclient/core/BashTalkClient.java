b0361f478478fbf7fa18218a80951c6e01474eb0
Jack <jmcker@outlook.com>
2017-10-20 15:11:38 -0400

    Initial commit

---
+++ src/com/bashtalkclient/core/BashTalkClient.java
@@ -0,0 +1,126 @@
+package com.bashtalkclient.core;
+
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.net.Socket;
+
+import javax.swing.JFrame;
+import javax.swing.JOptionPane;
+import javax.swing.JScrollPane;
+import javax.swing.JTextArea;
+import javax.swing.JTextField;
+import javax.swing.text.DefaultCaret;
+
+
+public class BashTalkClient {
+
+    private String host;
+    private final int PORT = 9898;
+    private String username;
+    private Socket socket;
+    private BufferedReader in;
+    private PrintWriter out;
+    private JFrame frame = new JFrame("Capitalize Client");
+    private JTextField dataField = new JTextField(40);
+    private JTextArea messageArea = new JTextArea(8, 60);
+
+    public BashTalkClient() {
+
+        // Layout GUI
+        messageArea.setEditable(false);
+        frame.getContentPane().add(dataField, "South");
+        frame.getContentPane().add(new JScrollPane(messageArea), "Center");
+        DefaultCaret caret = (DefaultCaret)messageArea.getCaret();
+        caret.setUpdatePolicy(DefaultCaret.ALWAYS_UPDATE);
+        
+        // Listen for enter in the text area (demo only until integrated with @patel760)
+        dataField.addActionListener(new ActionListener() {
+            public void actionPerformed(ActionEvent e) {
+                String data = dataField.getText();
+                if (data.indexOf("$$exit$$") != -1) {
+                    try {
+                        socket.close();
+                    } catch (Exception err) {
+                        messageArea.append("Could not close socket.\n");
+                    }
+                    System.exit(0);
+                }
+                
+                out.println(username + ": " + data);
+                dataField.setText("");
+            }
+        });
+    }
+
+    
+    public void connectToServer() throws IOException {
+
+        // Get the server address and from a dialog box.
+        //TODO: Needs Regex from @patel760
+        host = JOptionPane.showInputDialog(frame, "Enter IP Address of the Server:", "Welcome to BashTalk", JOptionPane.QUESTION_MESSAGE);
+        if (host == null) {
+            System.exit(0);
+        }
+        
+        // Make connection and initialize streams
+        socket = new Socket(host, PORT);
+        in = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+        out = new PrintWriter(socket.getOutputStream(), true);
+        
+        // Prompt for username until server validates
+        while (true) {
+            // "Please enter a valid username: "
+            String response = in.readLine();
+            messageArea.append(response + "\n");
+            
+            username = JOptionPane.showInputDialog(frame, "Enter username for " + host + "[" + PORT + "]:", "BashTalk Server " + host + "[" + PORT + "]", JOptionPane.QUESTION_MESSAGE);
+            if (username == null) {
+                System.exit(0);
+            }
+            out.println(username);
+            
+            response = in.readLine();
+            
+            // Username has been accepted and server join is successful
+            if (response.equals("Username approved. Welcome.")) {
+                messageArea.setText("");
+                messageArea.append(response + "\n");
+                
+                // Receive cached messages
+                while (!response.equals("-- End of Message History --")) {
+                    response = in.readLine();
+                    messageArea.append(response + "\n");
+                }
+                break;
+            } else {
+                JOptionPane.showMessageDialog(frame, response);
+            }
+        }
+
+        // Listen for messages, filter special commands, and append to display
+        while (true) {
+            String incoming = in.readLine();
+            if (incoming == null || incoming.indexOf("$$shutdown$$") != -1) {
+                messageArea.setText("");
+                System.exit(0);
+                break;
+            } else if (incoming.indexOf("$$clear$$")  != -1) {
+                messageArea.setText("");
+                continue;
+            }
+            messageArea.append(incoming + "\n");
+        }
+    }
+
+    public static void main(String[] args) throws Exception {
+        BashTalkClient client = new BashTalkClient();
+        client.frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        client.frame.pack();
+        client.frame.setVisible(true);
+        client.connectToServer();
+    }
+}
---
+++ src/com/bashtalkserver/core/BashTalkServer.java
@@ -0,0 +1,156 @@
+package com.bashtalkserver.core;
+
+import java.util.ArrayList;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.net.URL;
+    
+public class BashTalkServer {
+    
+    private static final int PORT = 9898;
+    private static ArrayList<Client> clients = new ArrayList<Client>();
+    private static ArrayList<String> messageCache = new ArrayList<String>();
+    private static final int MAX_CLIENTS = 50;
+
+    
+    public static void main(String[] args) throws Exception {
+        final String HOST = getIp();
+        int clientNumber = 0;
+        
+        System.out.print("\033[H\033[2J"); // "clear" command
+        System.out.println("-- BashTalk Server " + HOST + "[" + PORT + "] --");
+        
+        ServerSocket listener = new ServerSocket(PORT);
+        try {
+            while (true) {
+                if (clients.size() < MAX_CLIENTS) {
+                    clients.add(new Client(clientNumber++, listener.accept()));
+                    clients.get(clients.size() - 1).start();
+                } else {
+                    Client temp = new Client(clientNumber + 1, listener.accept());
+                    temp.directMsg("Maximum number of clients reached.");
+                    temp.close(false);
+                }
+            }
+        } finally {
+            broadcastMsg("shutdown");
+            listener.close();
+        }
+    }
+    
+    public static class Client extends Thread {
+        public int clientNumber;
+        public String username;
+        private BufferedReader in;
+        private PrintWriter out;
+        private Socket socket;
+        
+        public Client(int clientNumber, Socket socket) {
+            this.clientNumber = clientNumber;
+            this.socket = socket;
+        }
+        
+        public void run() {
+            try {
+                // Initialize streams
+                in = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+                out = new PrintWriter(socket.getOutputStream(), true);
+                
+                // Handle username
+                while (true) {
+                    this.directMsg("Please enter a valid username: ");
+                    String username = in.readLine();
+                    
+                    if (username.length() > 0) {
+                        this.directMsg("Username approved. Welcome.");
+                        log(username + " has joined the server as client #" + this.clientNumber + ".");
+                        this.username = username;
+                        break;
+                    }
+                }
+                
+                // Send cached messages
+                for (String msg : messageCache) {
+                    this.directMsg(msg);
+                }
+                this.directMsg("-- End of Message History --");
+                broadcastMsg(username + " has joined the server.");
+
+                // Wait for messages from client
+                while (true) {
+                    String msg = in.readLine();
+                    
+                    if (msg.indexOf("$$exit$$") != -1) {
+                        clients.remove(this);
+                        this.close(true);
+                        break;
+                    } else if (msg.indexOf("$$clear$$") != -1) {
+                        messageCache.clear();
+                        broadcastMsg(msg);
+                        continue;
+                    }
+                    
+                    broadcastMsg(msg); 
+                    messageCache.add(msg);
+                }
+            } catch (IOException e) {
+                log("Error handling client #" + this.clientNumber + ": " + e);
+            } finally {
+                this.close(true);
+            }
+        }
+        
+        public void directMsg(String msg) {
+            this.out.println(msg);
+        }
+        
+        public void close(boolean notify) {
+            try {
+                this.in.close();
+                this.out.close();
+                this.socket.close();
+                if (notify) {
+                    broadcastMsg(this.username + " has left the server.");
+                }
+            } catch (Exception e) {
+                log("Error closing socket #" + this.clientNumber + ": " + e);
+            }
+        }
+        
+        private void log(String msg) {
+            System.out.println(msg);
+        }
+    }
+    
+    private static void broadcastMsg(String msg) {
+        for (Client client : clients) {
+            try {
+                client.directMsg(msg);
+            }
+            catch (Exception e) {
+                System.out.println("Error sending message \"" + msg + "\": " + e);
+            }
+        }
+    }
+    
+    public static String getIp() throws Exception {
+        URL AWSCheck = new URL("http://checkip.amazonaws.com");
+        BufferedReader in = null;
+        try {
+            in = new BufferedReader(new InputStreamReader(AWSCheck.openStream()));
+            return in.readLine();
+        } finally {
+            if (in != null) {
+                try {
+                    in.close();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+        }
+    }
+}
