3cd516521db249647e927e02a600aae388daf88d
Neel <patelneel55@gmail.com>
2017-10-22 20:33:52 -0400

    Input accessbility features fixed and comments added

--- src/bashtalkclient/ui/ChatUI.java
+++ src/bashtalkclient/ui/ChatUI.java
@@ -2,235 +2,243 @@ package bashtalkclient.ui;
 
 import java.awt.*;
 import java.awt.event.*;
-import java.io.File;
-import java.io.IOException;
-import java.util.regex.*;
+
+import javax.swing.*;
+import javax.swing.border.*;
+import javax.swing.text.*;
+
+import java.io.*;
 
 import bashtalkclient.core.*;
 
-import javax.imageio.ImageIO;
-import javax.swing.*;
-import javax.swing.border.*;
-import javax.swing.text.BadLocationException;
-import javax.swing.text.DefaultCaret;
-import javax.swing.text.JTextComponent;
+public class ChatUI extends JFrame 
+{
+	
+	// Initialize final variabless
+	private static final int WIDTH = 89;
+	private static final int HEIGHT = 49;
 
-public class ChatUI extends JFrame {
+	// Instantiate major JPanels and Windows
+	private Window window;
+	private JPanel contentPane, inputPanel;
+	
+	// Instantiate JComponents and other variables
+	private Font font;
+	private JTextArea terminal, input;
+	private JLabel tag;
+	public String username;
 
-    private static final int WIDTH = 89;
-    private static final int HEIGHT = 49;
+	// Instance of a client
+	private BashTalkClient client;
 
-    private Window window;
-    public String username = "#username: ";
-    private Font font;
-    private JPanel contentPane, inputPanel;
-    private JTextArea terminal, input;
-    private JLabel tag;
+	public ChatUI(String usr, BashTalkClient client) 
+	{
+		// Format username input
+		username = "<" + usr + "> ";
+		
+		// Calculates scaling unique to each resolution screen
+		GraphicsDevice gd = GraphicsEnvironment.getLocalGraphicsEnvironment().getDefaultScreenDevice();
+		int uiscale = (int) Math.round((gd.getDisplayMode().getHeight() / 100.0) + 0.5);
+		int scale = (gd.getDisplayMode().getHeight() + gd.getDisplayMode().getWidth()) / 200;
+		
+		// Initialize client and window
+		this.client = client;
+		window = this;
+		
+		// Sets up intial text and accessibility features
+		setTitle("BashTalk");
+		setResizable(false);
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		setResolution(uiscale, scale);
+		setLocationRelativeTo(null);
 
-    private BashTalkClient client;
+		// Initializes the login components
+		initComponents();
+		setContentPane(contentPane);
 
-    public ChatUI(String usr, BashTalkClient client) {
-        username = "<" + usr + "> ";
-        this.client = client;
+		// Runs the KeyListener to check if Enter is pressed
+		try {
+			keyPress();
+		} catch (Exception e) {
+			System.out.println("IO Error");
+		}
 
-        // Calculates scaling unique to each resolution screen
-        GraphicsDevice gd = GraphicsEnvironment.getLocalGraphicsEnvironment().getDefaultScreenDevice();
-        int uiscale = (int) Math.round((gd.getDisplayMode().getHeight() / 100.0) + 0.5);
-        int scale = (gd.getDisplayMode().getHeight() + gd.getDisplayMode().getWidth()) / 200;
+	}
 
-        window = this;
+	/* Sets the resolution of the UI */
+	public void setResolution(int uiscale, int scale) 
+	{
+		setSize(WIDTH * uiscale, HEIGHT * uiscale);
+		font = new Font("Consolas", Font.PLAIN, scale);
+	}
 
-        // Sets up intial text and accessibility features
-        setTitle("BashTalk");
-        setResizable(false);
-        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        setResolution(uiscale, scale);
-        setLocationRelativeTo(null);
+	/* Initializes all JComponents to displayed to the client */
+	private void initComponents() 
+	{
+		// Creates the program icon and sets it to the JFrame
+		ImageIcon img = new ImageIcon("src/bashTalk.png");
+		this.setIconImage(img.getImage());
+		
+		// Creates a panel for the whole JFrame
+		contentPane = new JPanel();
+		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
+		contentPane.setLayout(new BoxLayout(contentPane, BoxLayout.Y_AXIS));
+		
+		// Settings for the username tag
+		tag = new JLabel(username);
+		tag.setFont(font);
+		tag.setOpaque(true);
+		tag.setBackground(Color.BLACK);
+		tag.setForeground(Color.GREEN);
+		tag.setBorder(BorderFactory.createLineBorder(Color.WHITE));
 
-        // Initializes the login components
-        initComponents(uiscale, scale);
-        setContentPane(contentPane);
+		// Settings for the input text area
+		input = new JTextArea(2, 50);
+		input.setFont(font);
+		input.setBackground(Color.BLACK);
+		input.setForeground(Color.WHITE);
+		input.setCaretColor(Color.WHITE);
+		input.setLineWrap(true);
+		input.setWrapStyleWord(true);
+		input.setCaret(new MyCaret());
 
-        // Runs the KeyListener to check if Enter is pressed
-        try {
-            keyPress();
-        } catch (Exception e) {
-            System.out.println("IO Error");
-        }
+		// Settings for the inputPanel that holds the username tag and the input textArea
+		inputPanel = new JPanel(new BorderLayout());
+		inputPanel.setOpaque(true);
+		inputPanel.setBackground(Color.black);
+		inputPanel.add(tag, BorderLayout.BEFORE_LINE_BEGINS);
+		inputPanel.add(new JScrollPane(input), BorderLayout.CENTER);
 
-    }
+		// Settings for the terminal textArea
+		terminal = new JTextArea(29, 131);
+		terminal.setFont(font);
+		terminal.setBackground(Color.BLUE);
+		terminal.setForeground(Color.GREEN);
+		terminal.setEditable(false);
+		
+		// Sets the scrollbar automatically to the bottom of the terminal textArea
+		DefaultCaret c = (DefaultCaret) terminal.getCaret();
+		c.setUpdatePolicy(DefaultCaret.ALWAYS_UPDATE);
 
-    /* Sets the resolution of the UI */
-    public void setResolution(int uiscale, int scale) {
-        setSize(WIDTH * uiscale, HEIGHT * uiscale);
-        font = new Font("Consolas", Font.PLAIN, scale);
-    }
+		// Settings for the terminalPanel that holds the terminal textArea
+		JPanel tpan = new JPanel(new BorderLayout());
+		tpan.add(new JScrollPane(terminal), BorderLayout.CENTER);
 
-    /* Initializes all JComponents to displayed to the client */
-    private void initComponents(int uiscale, int scale) {
-        // Creates the program icon and sets it to the JFrame
-    	ImageIcon img = new ImageIcon("src/bashTalk.png");
-        this.setIconImage(img.getImage());
-        
-        contentPane = new JPanel();
-        contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
-        contentPane.setLayout(new BoxLayout(contentPane, BoxLayout.Y_AXIS));
+		// ContentPane holds the terminal Panel and the inputPanel
+		contentPane.add(tpan, "Center");
+		contentPane.add(inputPanel, "South");
 
-        // Settings for the username tag
-        tag = new JLabel(username);
-        tag.setFont(font);
-        tag.setOpaque(true);
-        tag.setBackground(Color.BLACK);
-        tag.setForeground(Color.GREEN);
-        tag.setBorder(BorderFactory.createLineBorder(Color.WHITE));
-        ;
+	}
 
-        // Settings for the input text area
-        input = new JTextArea(2, 50);
-        input.setFont(font);
-        input.setBackground(Color.BLACK);
-        input.setForeground(Color.WHITE);
-        input.setCaretColor(Color.WHITE);
-        input.setLineWrap(true);
-        input.setWrapStyleWord(true);
-        input.setCaret(new MyCaret());
+	/* Appends the provided message to the terminal screen */
+	public void addMessage(String msg)
+	{
+		terminal.setEditable(true);
+		terminal.append(msg + "\n");
+		terminal.setEditable(false);
+	}
 
-        // Settings for the inputPanel that holds the username tag and the input
-        // textArea
-        inputPanel = new JPanel(new BorderLayout());
-        inputPanel.setOpaque(true);
-        inputPanel.setBackground(Color.black);
-        inputPanel.add(tag, BorderLayout.BEFORE_LINE_BEGINS);
-        inputPanel.add(new JScrollPane(input), BorderLayout.CENTER);
+	/* Clears the terminal screen */
+	public void clear()
+	{
+		terminal.setEditable(true);
+		terminal.setText("");
+		terminal.setEditable(false);
+	}
 
-        // Settings for the terminal textArea
-        terminal = new JTextArea(29, 131);
-        terminal.setFont(font);
-        terminal.setBackground(Color.BLUE);
-        terminal.setForeground(Color.GREEN);
-        terminal.setEditable(false);
-        //Sets the scrollbar automatically to the bottom of the JTextArea
-        DefaultCaret c = (DefaultCaret)terminal.getCaret();
-        c.setUpdatePolicy(DefaultCaret.ALWAYS_UPDATE);
+	/*
+	 * Checks if Enter is pressed and runs pre-set commands or broadcasts the
+	 * message to other clients
+	 */
+	public void keyPress() throws IOException 
+	{
+		//Checks for keyPress in the input textArea
+		input.addKeyListener(new KeyListener() {
 
-        // Settings for the terminalPanel that holds the terminal textArea
-        JPanel tpan = new JPanel(new BorderLayout());
-        tpan.add(new JScrollPane(terminal), BorderLayout.CENTER);
+			@Override
+			public void keyPressed(KeyEvent e) {
+				if (e.isShiftDown() && e.getKeyCode() == KeyEvent.VK_ENTER)
+					input.append("\n");
+				else if (e.getKeyCode() == KeyEvent.VK_ENTER) 
+				{
+					// Forwards the message to the server when enter is pressed
+					if (input.getText().equals("/clear"))
+						clear();
+					else
+						client.sendMessage(username + input.getText());
+					input.setText(null);
+				}
+			}
 
-        // ContentPane holds the terminal panel and the inputPanel
-        contentPane.add(tpan, "Center");
-        contentPane.add(inputPanel, "South");
+			@Override
+			public void keyTyped(KeyEvent e) {
+			}
 
-    }
+			@Override
+			public void keyReleased(KeyEvent e) {
+				if (e.getKeyCode() == KeyEvent.VK_ENTER && !e.isShiftDown())
+					input.setText(null);
+			}
+		});
+	}
 
-    /* Appends the provided message to the terminal screen */
-    public void addMessage(String msg) {
-        terminal.setEditable(true);
-        terminal.append(msg + "\n");
-        terminal.setEditable(false);
-    }
+	/* Creates a custom caret that gives a more terminal feel */
+	public class MyCaret extends DefaultCaret {
+		private java.awt.Image img = null;
 
-    /* Clears the terminal screen */
-    public void clear() {
-        terminal.setEditable(true);
-        terminal.setText("");
-        terminal.setEditable(false);
-    }
+		/* Initializes the new caret by setting its image and its blink rate */
+		public MyCaret() {
+			setBlinkRate(500);
+			try {
+				img = new ImageIcon("src/cursor.png").getImage();
+			} catch (Exception e) {
+				System.out.println("Cursor image not found!");
+			}
+		}
 
-    /*
-     * Checks if Enter is pressed and runs pre-set commands or broadcasts the
-     * message to other clients
-     */
-    public void keyPress() throws IOException {
-        input.addKeyListener(new KeyListener() {
+		/* Removes the current instance of the caret and repaints it to the terminal */
+		@Override
+		protected synchronized void damage(Rectangle r) {
+			if (r == null)
+				return;
 
-            @Override
-            public void keyPressed(KeyEvent e) {
-                if (e.isShiftDown() && e.getKeyCode() == KeyEvent.VK_ENTER)
-                    input.append("\n");
-                else if (e.getKeyCode() == KeyEvent.VK_ENTER) {
-                    // Forwards the message to the server when enter is pressed
-                    if (input.getText().equals("/clear"))
-                        clear();
-                    else 
-                        client.sendMessage(username + input.getText());
-                    input.setText(null);
-                }
-            }
+			FontMetrics fm = getComponent().getFontMetrics(getComponent().getFont());
+			x = r.x;
+			y = r.y;
+			width = tag.getFont().getSize() / 2;
+			height = tag.getFont().getSize();
+			repaint(); // calls getComponent().repaint(x, y, width, height)
+		}
 
-            @Override
-            public void keyTyped(KeyEvent e) {
-            }
+		/* Paints the caret image to the same location every blink interval */
+		@Override
+		public void paint(Graphics g) {
+			JTextComponent comp = getComponent();
+			if (comp == null)
+				return;
 
-            @Override
-            public void keyReleased(KeyEvent e) {
-                if (e.getKeyCode() == KeyEvent.VK_ENTER)
-                    input.setText(null);
-            }
-        });
-    }
+			int dot = getDot();
+			Rectangle r = null;
+			try {
+				r = comp.modelToView(dot);
+			} catch (BadLocationException e) {
+				return;
+			}
+			if (r == null)
+				return;
 
-    /* Creates a custom caret that gives a more terminal feel */
-    public class MyCaret extends DefaultCaret 
-    {
-        private java.awt.Image img = null;
-        
-        /*Initializes the new caret by setting its image and its blink rate*/
-        public MyCaret() 
-        {
-            setBlinkRate(500);
-            try {
-                img = new ImageIcon("src/cursor.png").getImage();
-            } catch (Exception e) {
-            	System.out.println("Cursor image not found!");
-            }
-        }
+			// If the location changes remove the current instance and repaint the caret
+			// symbol to its new location
+			if ((x != r.x) || (y != r.y)) {
+				repaint(); // erase previous location of caret
+				damage(r);
+			}
 
-        /*Removes the current instance of the caret and repaints it to the terminal*/
-        @Override
-        protected synchronized void damage(Rectangle r) 
-        {
-            if (r == null)
-                return;
+			// Draws the caret on the frame
+			if (isVisible())
+				g.drawImage(img, x, y + 2, width, height, Color.white, window);
+		}
 
-            FontMetrics fm = getComponent().getFontMetrics(getComponent().getFont());
-            x = r.x;
-            y = r.y;
-            width = tag.getFont().getSize() / 2;
-            height = tag.getFont().getSize();
-            repaint(); // calls getComponent().repaint(x, y, width, height)
-        }
-
-        /*Paints the caret image to the same location every blink interval*/
-        @Override
-        public void paint(Graphics g) 
-        {
-            JTextComponent comp = getComponent();
-            if (comp == null)
-                return;
-
-            int dot = getDot();
-            Rectangle r = null;
-            try {
-                r = comp.modelToView(dot);
-            } catch (BadLocationException e) {
-                return;
-            }
-            if (r == null)
-                return;
-
-            //If the location changes remove the current instance and repaint the caret symbol to its new location
-            if ((x != r.x) || (y != r.y)) 
-            {
-                repaint(); // erase previous location of caret
-                damage(r);
-            }
-
-            //Draws the caret on the frame
-            if (isVisible())
-                g.drawImage(img, x, y + 2, width, height, Color.white, window);
-        }
-
-    }
+	}
 
 }
