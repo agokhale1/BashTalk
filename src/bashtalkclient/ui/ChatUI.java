c6ee21673812c246c403f3220a180bce8bb03da4
Jack McKernan <jmcker@outlook.com>
2019-04-01 00:14:34 -0400

    Fix scanner issues; Temporarily disable resources that do not work
    Co-authored-by: Neel Patel <patel760@purdue.edu>

--- src/bashtalkclient/ui/ChatUI.java
+++ src/bashtalkclient/ui/ChatUI.java
@@ -12,61 +12,61 @@ import javax.swing.text.*;
 import bashtalkclient.core.*;
 
 public class ChatUI extends JFrame {
-	
+
 	private static final long serialVersionUID = 3246415652860859900L;
-	
+
 	// Initialize final variabless
 	private static final int WIDTH = 89;
 	private static final int HEIGHT = 49;
-	
+
 	// Instantiate major JPanels and Windows
 	private Window window;
 	private JPanel contentPane, inputPanel;
-	
+
 	// Instantiate JComponents and other variables
 	private Font font;
 	private JTextArea terminal, input;
 	private JLabel tag;
 	public String username;
-	
+
 	// Instance of a client
 	private BashTalkClient client;
-	
+
 	public ChatUI(String usr, BashTalkClient client)
 	{
 		// Format username input
 		this.username = "<" + usr + "> ";
-		
+
 		// Calculates scaling unique to each resolution screen
 		GraphicsDevice gd = GraphicsEnvironment.getLocalGraphicsEnvironment().getDefaultScreenDevice();
 		int uiscale = (int) Math.round(gd.getDisplayMode().getHeight() / 100.0 + 0.5);
 		int scale = (gd.getDisplayMode().getHeight() + gd.getDisplayMode().getWidth()) / 200;
-		
+
 		// Initialize client and window
 		this.client = client;
 		this.window = this;
-		
+
 		// Sets up intial text and accessibility features
 		setTitle("BashTalk");
 		setResizable(false);
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		setResolution(uiscale, scale);
 		setLocationRelativeTo(null);
-		
+
 		// Initializes the login components
 		initComponents();
 		setContentPane(this.contentPane);
-		
+
 		addWindowFocusListener(new WindowAdapter() {
-			
+
 			@Override
 			public void windowGainedFocus(WindowEvent e)
 			{
 				input.requestFocusInWindow();
 			}
-			
+
 		});
-		
+
 		// Runs the KeyListener to check if Enter is pressed
 		try
 		{
@@ -76,32 +76,32 @@ public class ChatUI extends JFrame {
 		{
 			System.out.println("IO Error");
 		}
-		
+
 	}
-	
+
 	/* Sets the resolution of the UI */
 	public void setResolution(int uiscale, int scale)
 	{
 		setSize(WIDTH * uiscale, HEIGHT * uiscale);
 		this.font = new Font("Consolas", Font.PLAIN, scale);
 	}
-	
+
 	/* Initializes all JComponents to displayed to the client */
 	private void initComponents()
 	{
 		// Creates the program icon and sets it to the JFrame
-		ArrayList<Image> iconImgs = new ArrayList<Image>();
-		
-		for(int i = 512; i >= 16; i /= 2)
-			iconImgs.add(new ImageIcon(getClass().getResource("/icon_" + i + "x" + i + ".png")).getImage());
-		
-		setIconImages(iconImgs);
-		
+		// ArrayList<Image> iconImgs = new ArrayList<Image>();
+
+		// for(int i = 512; i >= 16; i /= 2)
+		// 	iconImgs.add(new ImageIcon(getClass().getResource("/icon_" + i + "x" + i + ".png")).getImage());
+
+		// setIconImages(iconImgs);
+
 		// Creates a panel for the whole JFrame
 		this.contentPane = new JPanel();
 		this.contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
 		this.contentPane.setLayout(new BoxLayout(this.contentPane, BoxLayout.Y_AXIS));
-		
+
 		// Settings for the username tag
 		this.tag = new JLabel(this.username);
 		this.tag.setFont(this.font);
@@ -109,7 +109,7 @@ public class ChatUI extends JFrame {
 		this.tag.setBackground(Color.BLACK);
 		this.tag.setForeground(Color.GREEN);
 		this.tag.setBorder(BorderFactory.createLineBorder(Color.WHITE));
-		
+
 		// Settings for the input text area
 		this.input = new JTextArea(2, 50);
 		this.input.setFont(this.font);
@@ -119,35 +119,35 @@ public class ChatUI extends JFrame {
 		this.input.setLineWrap(true);
 		this.input.setWrapStyleWord(true);
 		this.input.setCaret(new MyCaret());
-		
+
 		// Settings for the inputPanel that holds the username tag and the input textArea
 		this.inputPanel = new JPanel(new BorderLayout());
 		this.inputPanel.setOpaque(true);
 		this.inputPanel.setBackground(Color.black);
 		this.inputPanel.add(this.tag, BorderLayout.BEFORE_LINE_BEGINS);
 		this.inputPanel.add(new JScrollPane(this.input), BorderLayout.CENTER);
-		
+
 		// Settings for the terminal textArea
 		this.terminal = new JTextArea(29, 131);
 		this.terminal.setFont(this.font);
 		this.terminal.setBackground(Color.BLUE);
 		this.terminal.setForeground(Color.GREEN);
 		this.terminal.setEditable(false);
-		
+
 		// Sets the scrollbar automatically to the bottom of the terminal textArea
 		DefaultCaret c = (DefaultCaret) this.terminal.getCaret();
 		c.setUpdatePolicy(DefaultCaret.ALWAYS_UPDATE);
-		
+
 		// Settings for the terminalPanel that holds the terminal textArea
 		JPanel tpan = new JPanel(new BorderLayout());
 		tpan.add(new JScrollPane(this.terminal), BorderLayout.CENTER);
-		
+
 		// ContentPane holds the terminal Panel and the inputPanel
 		this.contentPane.add(tpan, "Center");
 		this.contentPane.add(this.inputPanel, "South");
-		
+
 	}
-	
+
 	/* Appends the provided message to the terminal screen */
 	public void addMessage(String msg)
 	{
@@ -155,7 +155,7 @@ public class ChatUI extends JFrame {
 		this.terminal.append(msg + "\n");
 		this.terminal.setEditable(false);
 	}
-	
+
 	/* Clears the terminal screen */
 	public void clear()
 	{
@@ -163,7 +163,7 @@ public class ChatUI extends JFrame {
 		this.terminal.setText("");
 		this.terminal.setEditable(false);
 	}
-	
+
 	/*
 	 * Checks if Enter is pressed and runs pre-set commands or broadcasts the
 	 * message to other clients
@@ -172,17 +172,17 @@ public class ChatUI extends JFrame {
 	{
 		// Checks for keyPress in the input textArea
 		this.input.addKeyListener(new KeyListener() {
-			
+
 			String pass = "";
 			boolean flag = false;
-			
+
 			@Override
 			public void keyPressed(KeyEvent e)
 			{
-				
+
 				// Protects the input so that it is not stolen by wavering eyes
 				passwordProtected(e);
-				
+
 				if (e.isShiftDown() && e.getKeyCode() == KeyEvent.VK_ENTER)
 					ChatUI.this.input.append("\n");
 				else if (e.getKeyCode() == KeyEvent.VK_ENTER)
@@ -199,26 +199,26 @@ public class ChatUI extends JFrame {
 					}
 					else
 						ChatUI.this.client.sendMessage(ChatUI.this.input.getText());
-					
+
 					// Flags the process that the input needs to be password protected
 					if (ChatUI.this.input.getText().equals("/clear_cache") || ChatUI.this.input.getText().indexOf("/ban") != -1)
 						this.flag = true;
 					ChatUI.this.input.setText(null);
 				}
 			}
-			
+
 			@Override
 			public void keyTyped(KeyEvent e)
 			{}
-			
+
 			@Override
 			public void keyReleased(KeyEvent e)
 			{
 				if (e.getKeyCode() == KeyEvent.VK_ENTER && !e.isShiftDown())
 					ChatUI.this.input.setText(null);
-				
+
 			}
-			
+
 			/* Protects the input textfield by converting the characters to stars and still keeping its typed value */
 			private void passwordProtected(KeyEvent e)
 			{
@@ -234,16 +234,16 @@ public class ChatUI extends JFrame {
 			}
 		});
 	}
-	
+
 	/* Creates a custom caret that gives a more terminal feel */
 	public class MyCaret extends DefaultCaret {
-		
+
 		/**
-		 * 
+		 *
 		 */
 		private static final long serialVersionUID = -9116419070786218647L;
 		private java.awt.Image img = null;
-		
+
 		/* Initializes the new caret by setting its image and its blink rate */
 		public MyCaret()
 		{
@@ -257,21 +257,21 @@ public class ChatUI extends JFrame {
 				System.out.println("Cursor image not found!");
 			}
 		}
-		
+
 		/* Removes the current instance of the caret and repaints it to the terminal */
 		@Override
 		protected synchronized void damage(Rectangle r)
 		{
 			if (r == null)
 				return;
-			
+
 			this.x = r.x;
 			this.y = r.y;
 			this.width = ChatUI.this.tag.getFont().getSize() / 2;
 			this.height = ChatUI.this.tag.getFont().getSize();
 			repaint(); // calls getComponent().repaint(x, y, width, height)
 		}
-		
+
 		/* Paints the caret image to the same location every blink interval */
 		@Override
 		public void paint(Graphics g)
@@ -279,7 +279,7 @@ public class ChatUI extends JFrame {
 			JTextComponent comp = getComponent();
 			if (comp == null)
 				return;
-			
+
 			int dot = getDot();
 			Rectangle r = null;
 			try
@@ -292,7 +292,7 @@ public class ChatUI extends JFrame {
 			}
 			if (r == null)
 				return;
-				
+
 			// If the location changes remove the current instance and repaint the caret
 			// symbol to its new location
 			if (this.x != r.x || this.y != r.y)
@@ -300,7 +300,7 @@ public class ChatUI extends JFrame {
 				repaint(); // erase previous location of caret
 				damage(r);
 			}
-			
+
 			// Draws the caret on the frame
 			if (isVisible())
 				g.drawImage(this.img, this.x, this.y + 2, this.width, this.height, Color.white, ChatUI.this.window);
--- src/bashtalkclient/ui/LoginUI.java
+++ src/bashtalkclient/ui/LoginUI.java
@@ -14,47 +14,47 @@ import javax.swing.text.*;
 import bashtalkclient.core.*;
 
 public class LoginUI extends JFrame {
-	
+
 	private static final long serialVersionUID = -2816500385275412556L;
-	
+
 	// Initialize final variables
 	private static final int WIDTH = 26;
 	private static final int HEIGHT = 36;
 	private static final Pattern PATTERN = Pattern.compile("^(([01]?\\d\\d?|2[0-4]\\d|25[0-5])\\.){3}([01]?\\d\\d?|2[0-4]\\d|25[0-5])$");
-	
+
 	// Lists and variables to incorporate IP address autocomplete and drop-down menu
 	private static ArrayList<String> addressList = new ArrayList<String>();
 	private static ArrayList<String> filteredList = new ArrayList<String>();
 	private String IPString = "";
-	
+
 	// File that stores the list of previously entered IPs
 	// private File ipFile = new File(getClass().getClassLoader().getResource("ip_file.txt").getFile());
-	private File ipFile = new File(getClass().getClassLoader().getResource("ip_file.txt").getFile());
+	//private File ipFile = new File(getClass().getClassLoader().getResource("ip_file.txt").getFile());
 	private BufferedReader reader = null;
 	private BufferedWriter writer = null;
-	
+
 	// Instantiate major JPanels and Windows
 	private Window window;
 	private JPanel contentPane, btnPane;
 	private DocumentListener addressEvent;
-	
+
 	// Instantiate JComponents
 	private Font font;
 	private JLabel usernameLbl, addressLbl, portLbl;
 	private JTextField username, port;
 	private JComboBox<String> address;
 	private JButton confirm, cancel;
-	
+
 	public LoginUI()
 	{
 		// Calculates scaling unique to each screen resolution
 		GraphicsDevice gd = GraphicsEnvironment.getLocalGraphicsEnvironment().getDefaultScreenDevice();
 		int uiscale = (int) Math.round(gd.getDisplayMode().getHeight() / 100.0 + 0.5);
 		int scale = (gd.getDisplayMode().getHeight() + gd.getDisplayMode().getWidth()) / 200;
-		
+
 		// Initialize client and window
 		this.window = this;
-		
+
 		// Sets up intial text and accessibility features
 		setTitle("Login");
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
@@ -62,25 +62,25 @@ public class LoginUI extends JFrame {
 		setUIHints(scale);
 		setResizable(false);
 		setLocationRelativeTo(null);
-		
+
 		// Listens for escape key to quit the login
 		KeyboardFocusManager kfm = KeyboardFocusManager.getCurrentKeyboardFocusManager();
 		kfm.addKeyEventDispatcher(new KeyEventDispatcher() {
-			
+
 			@Override
 			public boolean dispatchKeyEvent(KeyEvent e)
 			{
-				
+
 				if (e.getID() == KeyEvent.KEY_PRESSED && e.getKeyCode() == KeyEvent.VK_ESCAPE)
 					dispatchEvent(new WindowEvent(LoginUI.this.window, WindowEvent.WINDOW_CLOSING));
 				return false;
 			}
 		});
-		
+
 		// Initializes the login components
 		initComponents();
 		setContentPane(this.contentPane);
-		
+
 		// Aligns all the JComponents and adds it to their respective JPanels
 		this.contentPane.add(Box.createVerticalStrut(scale * 3));
 		this.contentPane.add(this.usernameLbl);
@@ -91,52 +91,52 @@ public class LoginUI extends JFrame {
 		this.contentPane.add(Box.createVerticalStrut(scale * 3));
 		this.contentPane.add(this.portLbl);
 		this.contentPane.add(this.port);
-		
+
 		this.btnPane.add(this.confirm);
 		this.btnPane.add(Box.createHorizontalStrut(scale));
 		this.btnPane.add(this.cancel);
 		this.btnPane.setAlignmentX(Component.CENTER_ALIGNMENT);
-		
+
 		this.contentPane.add(Box.createVerticalStrut(scale * 3));
 		this.contentPane.add(this.btnPane);
-		
+
 		// Adds the values to the comboBox
-		initIPList();
+		//initIPList();
 	}
-	
+
 	/* Sets the resolution of the UI */
 	private void setResolution(int uiscale, int scale)
 	{
 		setSize(WIDTH * uiscale, HEIGHT * uiscale);
 		this.font = new Font("Consolas", Font.PLAIN, scale);
 	}
-	
+
 	/* Set the UI to the correct font based on the scale */
 	private void setUIHints(int scale)
 	{
 		UIManager.put("OptionPane.messageFont", new Font("Consolas", Font.PLAIN, scale * 2));
 		UIManager.put("OptionPane.buttonFont", new Font("Consolas", Font.BOLD, scale * 2));
 	}
-	
+
 	/* Initializes all JComponents to displayed to the client */
 	private void initComponents()
 	{
 		// Creates the program icon and sets it to the JFrame
-		ArrayList<Image> iconImgs = new ArrayList<Image>();
-		
-		for (int i = 512; i >= 16; i /= 2)
-			iconImgs.add(new ImageIcon(getClass().getResource("/icon_" + i + "x" + i + ".png")).getImage());
-		
-		setIconImages(iconImgs);
-		
+		//ArrayList<Image> iconImgs = new ArrayList<Image>();
+
+//		for (int i = 512; i >= 16; i /= 2)
+//			iconImgs.add(new ImageIcon(getClass().getResource("/icon_" + i + "x" + i + ".png")).getImage());
+
+		//setIconImages(iconImgs);
+
 		// Creates a panel for the whole JFrame
 		this.contentPane = new JPanel();
 		this.contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
 		this.contentPane.setLayout(new BoxLayout(this.contentPane, BoxLayout.Y_AXIS));
-		
+
 		// Creates a panel for all buttons
 		this.btnPane = new JPanel();
-		
+
 		// Creates the labels for the username, ip and port
 		this.usernameLbl = new JLabel("Enter a Username");
 		this.usernameLbl.setFont(this.font);
@@ -144,16 +144,16 @@ public class LoginUI extends JFrame {
 		this.addressLbl.setFont(this.font);
 		this.portLbl = new JLabel("Enter a Port");
 		this.portLbl.setFont(this.font);
-		
+
 		// Creates the text field for username input and validates the username
 		this.username = new JTextField(26);
 		this.username.setFont(this.font);
 		this.username.addFocusListener(new FocusListener() {
-			
+
 			@Override
 			public void focusLost(FocusEvent e)
 			{
-				
+
 				if (!LoginUI.this.username.getText().equals(""))
 					if (!validateUsername(LoginUI.this.username.getText()))
 					{
@@ -162,17 +162,17 @@ public class LoginUI extends JFrame {
 					}
 					else
 						LoginUI.this.username.setForeground(Color.GREEN);
-					
+
 			}
-			
+
 			@Override
 			public void focusGained(FocusEvent e)
 			{
 				LoginUI.this.username.setForeground(Color.BLACK);
 			}
-			
+
 		});
-		
+
 		// Creates the text field for the ip address and validates ip address
 		this.address = new JComboBox<String>();
 		this.address.setEditable(true);
@@ -193,14 +193,14 @@ public class LoginUI extends JFrame {
 					//System.out.println("this "+address.getEditor().getItem().toString());
 					//updateIPList();
 					SwingUtilities.invokeLater(new Runnable() {
-	
+
 						@Override
 						public void run()
 						{
 							System.out.println("this "+address.getEditor().getItem().toString());
 							updateIPList();
 						}
-						
+
 					});
 				}
 				inInit = true;
@@ -213,9 +213,9 @@ public class LoginUI extends JFrame {
 				{
 					//System.out.println("that "+address.getEditor().getItem().toString());
 					//updateIPList();
-				
+
 					SwingUtilities.invokeLater(new Runnable() {
-	
+
 						@Override
 						public void run()
 						{
@@ -229,7 +229,7 @@ public class LoginUI extends JFrame {
 		};
 		((JTextField)this.address.getEditor().getEditorComponent()).getDocument().addDocumentListener(addressEvent);
 		this.address.getEditor().getEditorComponent().addFocusListener(new FocusListener() {
-			
+
 			@Override
 			public void focusLost(FocusEvent e)
 			{
@@ -242,40 +242,40 @@ public class LoginUI extends JFrame {
 					else
 						LoginUI.this.address.getEditor().getEditorComponent().setForeground(Color.GREEN);
 			}
-			
+
 			@Override
 			public void focusGained(FocusEvent e)
 			{
 				LoginUI.this.address.setForeground(Color.BLACK);
 			}
 		});
-		
+
 		// Creates a text field for the port number, validates the port number and connects to client on ENTER keypress
 		this.port = new JTextField(5);
 		this.port.setFont(this.font);
 		this.port.addKeyListener(new KeyListener() {
-			
+
 			@Override
 			public void keyPressed(KeyEvent e)
 			{
 				if (e.getKeyCode() == KeyEvent.VK_ENTER && validateIP(LoginUI.this.address.getSelectedItem().toString()) && validateUsername(LoginUI.this.username.getText()) && !validatePort(LoginUI.this.port.getText()))
 					createConnection();
 			}
-			
+
 			@Override
 			public void keyTyped(KeyEvent e)
 			{}
-			
+
 			@Override
 			public void keyReleased(KeyEvent e)
 			{}
 		});
 		this.port.addFocusListener(new FocusListener() {
-			
+
 			@Override
 			public void focusLost(FocusEvent e)
 			{
-				
+
 				if (!LoginUI.this.port.getText().equals(""))
 					if (validatePort(LoginUI.this.port.getText()))
 					{
@@ -284,22 +284,22 @@ public class LoginUI extends JFrame {
 					}
 					else
 						LoginUI.this.port.setForeground(Color.GREEN);
-					
+
 			}
-			
+
 			@Override
 			public void focusGained(FocusEvent e)
 			{
 				LoginUI.this.port.setForeground(Color.BLACK);
 			}
-			
+
 		});
-		
+
 		// Creates the Confirm button and connects to client onClick
 		this.confirm = new JButton("Confirm");
 		this.confirm.setFont(this.font);
 		this.confirm.addActionListener(new ActionListener() {
-			
+
 			@Override
 			public void actionPerformed(ActionEvent e)
 			{
@@ -307,37 +307,37 @@ public class LoginUI extends JFrame {
 					createConnection();
 			}
 		});
-		
+
 		// Creates the cancel button and closes the LoginUI onClick
 		this.cancel = new JButton("Cancel");
 		this.cancel.setFont(this.font);
 		this.cancel.addActionListener(new ActionListener() {
-			
+
 			@Override
 			public void actionPerformed(ActionEvent e)
 			{
 				LoginUI.this.window.dispose();
 			}
 		});
-		
+
 		// Aligns the JComponents to their desired positions
 		this.usernameLbl.setAlignmentX(Component.CENTER_ALIGNMENT);
 		this.addressLbl.setAlignmentX(Component.CENTER_ALIGNMENT);
 		this.portLbl.setAlignmentX(Component.CENTER_ALIGNMENT);
-		
+
 		this.username.setAlignmentX(Component.CENTER_ALIGNMENT);
 		this.username.setMaximumSize(this.username.getPreferredSize());
-		
+
 		this.address.setAlignmentX(Component.CENTER_ALIGNMENT);
 		this.address.setMaximumSize(new Dimension(370, 200));
-		
+
 		this.port.setAlignmentX(Component.CENTER_ALIGNMENT);
 		this.port.setMaximumSize(this.port.getPreferredSize());
-		
+
 		this.cancel.setPreferredSize(this.confirm.getPreferredSize());
-		
+
 	}
-	
+
 	/* Attempts the connection between the client and server else throws its respective exception */
 	public void createConnection()
 	{
@@ -345,115 +345,115 @@ public class LoginUI extends JFrame {
 		{
 			this.address.setSelectedItem("127.0.0.1");
 		}
-		
+
 		// Create UI Client
 		new ClientUIMode(this.address.getSelectedItem().toString(), this.port.getText(), this.username.getText());
-		
+
 		this.window.dispose();
-		if (!addressList.contains(address.getSelectedItem()))
-		{
-			addressList.add(IPString);
-			address.addItem(IPString);
-			try
-			{
-				writer = new BufferedWriter(new FileWriter(getClass().getClassLoader().getResource("ip_file.txt").getFile(), true));
-				writer.append(address.getSelectedItem() + "\n");
-				writer.close();
-			}
-			catch (IOException e)
-			{
-				e.printStackTrace();
-			}
-		}
+		// if (!addressList.contains(address.getSelectedItem()))
+		// {
+		// 	addressList.add(IPString);
+		// 	address.addItem(IPString);
+		// 	try
+		// 	{
+		// 		writer = new BufferedWriter(new FileWriter(getClass().getClassLoader().getResource("ip_file.txt").getFile(), true));
+		// 		writer.append(address.getSelectedItem() + "\n");
+		// 		writer.close();
+		// 	}
+		// 	catch (IOException e)
+		// 	{
+		// 		e.printStackTrace();
+		// 	}
+		// }
 	}
-	
+
 	/* Validates the IP Address to its correct format */
 	private boolean validateIP(String ip)
 	{
 		return PATTERN.matcher(ip).matches() || ip.equals("localhost");
 	}
-	
+
 	/* Validates username excluding all special characters */
 	private boolean validateUsername(String username)
 	{
 		return username.matches("^[a-zA-Z0-9]*$");
 	}
-	
+
 	/* Validates the port number by accepting only numbers */
 	private boolean validatePort(String port)
 	{
 		return !port.matches("[0-9]+");
 	}
-	
+
 	/* Initializes the address list with the previously used IP's stored in the ipFile */
 	private void initIPList()
 	{
-		try
-		{
-			reader = new BufferedReader(new FileReader(ipFile));
-			String inLine = "";
-			
-			while ((inLine = reader.readLine()) != null)
-			{
-				addressList.add(inLine);
-				address.addItem(inLine);
-			}
-		}
-		catch (FileNotFoundException e)
-		{
-			e.printStackTrace();
-		}
-		catch (IOException e)
-		{
-			e.printStackTrace();
-		}
-		finally
-		{
-			try
-			{
-				if (reader != null)
-					reader.close();
-			}
-			catch (Exception e)
-			{
-				e.printStackTrace();
-			}
-		}
+//		try
+//		{
+//			reader = new BufferedReader(new FileReader(ipFile));
+//			String inLine = "";
+//
+//			while ((inLine = reader.readLine()) != null)
+//			{
+//				addressList.add(inLine);
+//				address.addItem(inLine);
+//			}
+//		}
+//		catch (FileNotFoundException e)
+//		{
+//			e.printStackTrace();
+//		}
+//		catch (IOException e)
+//		{
+//			e.printStackTrace();
+//		}
+//		finally
+//		{
+//			try
+//			{
+//				if (reader != null)
+//					reader.close();
+//			}
+//			catch (Exception e)
+//			{
+//				e.printStackTrace();
+//			}
+//		}
 		address.setSelectedItem("");
 	}
-	
+
 	/* Updates the IP list to the entered value and auto-completes through the previously used IPs */
 	private void updateIPList()
 	{
 		// Disables document listener so that address ComboBox can be altered
 		((JTextField)this.address.getEditor().getEditorComponent()).getDocument().removeDocumentListener(addressEvent);
-		
+
 		address.hidePopup();
 		filteredList.clear();
-		
+
 		int numItems = address.getItemCount();
 		String typed = address.getEditor().getItem().toString();
-		
+
 		// Removes all the IP data so that it can be filtered
 		for (int i = 0; i < numItems; i++)
 			address.removeItemAt(0);
 		address.setSelectedItem(typed);
-		
+
 		// Determines which IP matches and stores it in filteredList
 		for(String addr : addressList)
 		{
 			if(addr.length() >= typed.length() && addr.substring(0, typed.length()).equals(typed))
 				filteredList.add(addr);
 		}
-		
+
 		// Repaints the filtered data onto the JComboBox
 		for(String filter : filteredList)
 		{
 			address.addItem(filter);
 		}
-		
+
 		address.showPopup();
-		
+
 		// Re-enables document listener so that it can listen for user changes and input
 		((JTextField)this.address.getEditor().getEditorComponent()).getDocument().addDocumentListener(addressEvent);
 	}
